3eb738d32a0636a6a408855d03e0ae813cd9b794
RomkaFCDK <99875803+RomkaFCDK@users.noreply.github.com>
2023-03-12 20:17:39 +0200

    Update SerialFile.java

--- Annotation/src/main/java/SerialFile.java
+++ Annotation/src/main/java/SerialFile.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class SerialFile {
     @Save
     private String string;
@@ -15,10 +17,16 @@ public class SerialFile {
         this.e = e;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException,ClassNotFoundException,IllegalAccessException {
         SerialFile sf = new SerialFile("Hello",3,7.7);
         Serializator.serialization(sf,"myfile.txt");
-        SerialFile sf2 = (SerialFile) Serializator.deserialization(SerialFile.class, "myfile.txt");
+        System.out.println(sf.string);
+        System.out.println(sf.d);
+        System.out.println(sf.e);
+
+
+        SerialFile sf2 = new SerialFile();
+        Serializator.deserialization(sf2,"myfile.txt");
         System.out.println(sf2.string);
         System.out.println(sf2.d);
         System.out.println(sf2.e);
