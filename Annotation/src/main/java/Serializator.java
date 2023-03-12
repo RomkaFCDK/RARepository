7d88f4bbe5f541287bcaa7d7ae931c261d13cabd
RomkaFCDK <99875803+RomkaFCDK@users.noreply.github.com>
2023-03-12 20:16:59 +0200

    Update Serializator.java

--- Annotation/src/main/java/Serializator.java
+++ Annotation/src/main/java/Serializator.java
@@ -1,36 +1,70 @@
 import java.io.*;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class Serializator {
-    public static void serialization(Object obj, String filename){
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+    public static void serialization(Object obj, String filename) throws IOException, IllegalAccessException {
+        try (FileOutputStream fos = new FileOutputStream(filename);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             DataOutputStream dos = new DataOutputStream(bos)) {
             Field[] fields = obj.getClass().getDeclaredFields();
             for (Field field : fields) {
                 if (field.isAnnotationPresent(Save.class)) {
                     field.setAccessible(true);
-                    oos.writeObject(field.get(obj));
+                    dos.writeUTF(field.getName());
+                    dos.writeUTF(field.getType().getName());
+                    dos.writeUTF(String.valueOf(field.get(obj)));
                 }
             }
-        }catch(IOException | IllegalAccessException e) {
-            e.printStackTrace();
         }
     }
 
-    public static Object deserialization(Class<?> clazz, String filename){
-        Object obj = null;
-        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            obj = clazz.newInstance();
-            Field[] fields = clazz.getDeclaredFields();
+
+    public static void deserialization(Object obj, String filename) throws IOException, IllegalAccessException, ClassNotFoundException {
+        try (FileInputStream fis = new FileInputStream(filename);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             DataInputStream dis = new DataInputStream(bis)) {
+            Field[] fields = obj.getClass().getDeclaredFields();
             for (Field field : fields) {
-            if(field.isAnnotationPresent(Save.class)) {
-                field.setAccessible(true);
-                Object thisField = ois.readObject();
-                field.set(obj,thisField);
+                if (field.isAnnotationPresent(Save.class)) {
+                    field.setAccessible(true);
+                    String nameField = dis.readUTF();
+                    if (nameField.equals(field.getName())) {
+                        String typeField = dis.readUTF();
+                        if (!typeField.equals(field.getType().getName())) {
+                            throw new IllegalStateException("Type field error");
+                        }
+                        String valueField = dis.readUTF();
+                        field.set(obj, parsingValue(field.getType(), valueField));
+                    } else {
+                        throw new IllegalStateException("Name field error");
+                    }
+                }
             }
-            }
-        }catch(IOException | IllegalAccessException | InstantiationException | ClassNotFoundException e){
-            e.printStackTrace();
         }
-        return obj;
+     }
+        private static Object parsingValue (Class < ? > typeField, String valueField){
+            if (typeField == boolean.class) {
+                return Boolean.parseBoolean(valueField);
+            } else if (typeField == short.class) {
+                return Short.parseShort(valueField);
+            } else if (typeField == byte.class) {
+                return Byte.parseByte(valueField);
+            } else if (typeField == int.class) {
+                return Integer.parseInt(valueField);
+            } else if (typeField == long.class) {
+                return Long.parseLong(valueField);
+            } else if (typeField == float.class) {
+                return Float.parseFloat(valueField);
+            } else if (typeField == double.class) {
+                return Double.parseDouble(valueField);
+            } else if (typeField == char.class) {
+                return valueField.charAt(0);
+            } else if (typeField == String.class) {
+                return valueField;
+            } else {
+                throw new IllegalArgumentException("Error field type" + typeField.getName());
+            }
+
+        }
     }
-}
